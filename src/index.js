bd824a849f3460a14a41e06dad0b584b8a541a23
rkrobby19 <rizkyrobby19@gmail.com>
2023-05-01 17:43:30 +0700

    Initial file config

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get("/", (req,res)=>{
+  res.send("Hello world")
+})
+
+const PORT = process.env.PORT || 8000
+
+const DB_URI = process.env.DB_URI
+mongoose.connect(DB_URI)
+.then(()=>{
+app.listen(PORT,()=>{
+  console.log(`Server is running on http://localhost:${PORT}`)})})
+  .catch(error=>console.log(error.name))
